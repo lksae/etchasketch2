3c10ec15152a6aa50dc11505027773fb2778566d
Lksae <matthiasschmitz21@gmail.com>
2024-06-10 10:54:05 +0200

    starting with extended functionalities

--- script.js
+++ script.js
@@ -14,16 +14,35 @@ if(windowHeight > windowWidth) {
 containerDiv.style.width = containerDivSize + "px";
 containerDiv.style.height = containerDivSize + "px";
 
+const gridNumbers = 16;
 
 
-let gridItemsSize = (containerDivSize  - (16 * 4)) / 16   ; 
+let gridItemsSize = (containerDivSize  - (gridNumbers * 4)) / gridNumbers   ; 
 console.log(containerDivSize);
 console.log(gridItemsSize);
 
-for (let i = 0; i < 256; i++) {
-    const gridDiv = document.createElement("div");
-    gridDiv.classList.add("grid");
-    gridDiv.style.width = gridItemsSize +"px";
-    gridDiv.style.height = gridItemsSize + "px";
-    containerDiv.appendChild(gridDiv);
+
+function createGrid(grids) {
+    for (let i = 1; i <= grids*grids; i++) {
+        const gridDiv = document.createElement("div");
+        gridDiv.classList.add("grid");
+        gridDiv.style.width = gridItemsSize +"px";
+        gridDiv.style.height = gridItemsSize + "px";
+        containerDiv.appendChild(gridDiv);
+        gridDiv.addEventListener("mouseover", event => {
+            gridDiv.style.backgroundColor = "black";
+        })
+    }
 }
+
+// get input number for squares
+const inputField = document.querySelector(".input");
+
+// restart sketch pad
+const restartButton = document.querySelector(".restartButton")
+restartButton.addEventListener("click", event => {
+    inputField = document.querySelector(".input");
+    // delete old grid and create new one
+})
+
+createGrid(gridNumbers)
